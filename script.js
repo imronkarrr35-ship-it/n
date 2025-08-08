d2b6ef4a711bf8ec8d4570c1162e098318fc754e
imronkarrr35-ship-it <imronkarrr35@gmail.com>
2025-08-08 15:04:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1-savol Math metodlar
+
+// let a = Math.trunc(Math.random()*10)
+
+// alert(a)
+
+
+
+// 2-savol Math metodlari
+
+// let b = prompt('weithe namber')
+
+// console.log(Math.trunc(b));
+
+
+// 3-savol for loop
+
+// let data = prompt('MARS IT SCHOOL')
+
+
+// console.log(data);
+
+
+// for(i=0 idata.length i==)(
+//     alert(data)
+    
+// )
+
+
+// 4-savol if else
+    let a = prompt('yosh in 18 dan')
+
+
+if(a = true ){
+    alert('18 dan kota')
+}else{
+    alert('18 dan kchik')
+}
+
+
+
+console.log(a);
